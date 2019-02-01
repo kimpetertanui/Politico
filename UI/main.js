aa5271d7082bf3188d53211fe14dedafbf97192c
kimpetertanui <kimelipeter526@gmail.com>
2019-02-01 04:17:50 +0300

    Modified the pages

---
+++ UI/main.js
@@ -0,0 +1,5 @@
+function getAndDisplay() {
+	var button=document.createElement("button");
+    var x = document.getElementById("myTextarea").value;
+    document.getElementById("display").innerHTML = x;
+}
