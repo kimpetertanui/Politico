642eb04a0bbafe480c6855d0173d8a26e18ddf7b
kimpetertanui <kimelipeter526@gmail.com>
2019-02-06 12:15:30 +0300

    Added create party endpoint

--- app/api/v1/views/views_party.py
+++ app/api/v1/views/views_party.py
@@ -11,21 +11,23 @@ def hello():
     return 'Flask API endpoint'
 
 
-@b_party.route("/getAllParties",methods=['GET'])
-def getAllParties():
-    return make_response(jsonify(parties), 200)
+# @b_party.route("/getAllParties",methods=['GET'])
+# def getAllParties():
+#     return make_response(jsonify(
+#         {"data":parties},
+#         {"status":200}))
 
 
-@b_party.route("/getParty/<partyID>",methods=['GET'])
-def getParty(partyID):
-    for party in parties:
-        if party["id"] == int(partyID):
-            return make_response(jsonify(party), 200)
-
-    return make_response(jsonify({
-        "code": 404,
-        "error": "Could not find party with id {}".format(partyID)
-    }), 404)
+# @b_party.route("/getParty/<partyID>",methods=['GET'])
+# def getParty(partyID):
+#     for party in parties:
+#         if partyID == partyID:
+#             return make_response(jsonify(party), 200)
+#         else:
+#             return make_response(jsonify({
+#                 "status": 404,
+#                 "error": "Could not find party with id {}".format(partyID)
+#             }), 404)
 
 
 @b_party.route("/addparty", methods=['POST'])
@@ -52,20 +54,20 @@ def addparty():
     }), 200)
 
 
-@b_party.route("/deleteParty/<partyID>", methods=['DELETE'])
-def deleteParty(partyID):
-    for party in parties:
-        if party["partyID"] == int(partyID):
-            parties.remove(party)
-            return make_response(jsonify({
-                "status": 200,
-                "data": "deleted successfully"
-            }), 200)
-
-    return make_response(jsonify({
-        "status": 404,
-        "error": "could not find party with ID {}".format(partyID)
-    }), 404)
+# @b_party.route("/deleteParty/<partyID>", methods=['DELETE'])
+# def deleteParty(partyID):
+#     for party in parties:
+#         if party["partyID"] == int(partyID):
+#             parties.remove(party)
+#             return make_response(jsonify({
+#                 "status": 200,
+#                 "data": "deleted successfully"
+#             }), 200)
+#
+#     return make_response(jsonify({
+#         "status": 404,
+#         "error": "could not find party with ID {}".format(partyID)
+#     }), 404)
 
 
 if __name__ == '__main__':
