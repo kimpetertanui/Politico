9f20479f32f6c9bcb4c01c46da43317c443221d8
kimpetertanui <kimelipeter526@gmail.com>
2019-02-16 19:03:12 +0300

    Added a test for badRequest,method not allowed and pageNotFound

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,30 @@ from app.api.v2.views.views_user_signup import v2
 
 from app.api.v1.views.views_party import b_party
 
+
+
+def badRequest(error):
+    return jsonify({
+        "error":str(error),
+        "status":404
+    }),404
+
+def methodNotallowed(error):
+    return jsonify({
+        "error":str(error),
+        "status":405
+    }),405
+def pageNotFound(error):
+    return jsonify({
+        "error":str(error),
+        "status":404
+    }),404
+
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
     app.register_blueprint(v1)
     app.register_blueprint(b_party)
+    app.register_error_handler(404,badRequest)
+    app.register_error_handler(405,methodNotallowed)
     app.register_blueprint(v2)
     return app
